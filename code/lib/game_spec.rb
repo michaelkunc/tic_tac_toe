930cb1374c59a67ecb81013141c3e5bf2c620df0
michaelkunc <mjkunc@gmail.com>
2015-08-30 15:46:41 -0500

    add some board and player tests

--- code/lib/game_spec.rb
+++ code/lib/game_spec.rb
@@ -1,3 +1,32 @@
 require_relative "game"
 
 
+
+
+describe "Board"  do
+  board = Board.new
+
+  it 'returns a board object' do
+  expect(board).to be_an_instance_of Board
+  end
+
+  it 'should have a length of 9' do
+    expect(board.board.length).to eq(9)
+  end
+
+  it 'should contain only "-" ' do
+    expect(board.board.all? {|e| e == "-"}).to eq(true)
+  end
+
+end
+
+
+describe "Player"  do
+  player = Player.new
+
+  it 'returns a Player object' do
+  expect(player).to be_an_instance_of Player
+  end
+
+end
+
