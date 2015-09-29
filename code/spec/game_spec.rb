93d0e0e76130b365e36e636f2ed1ebb475dcb5e9
michaelkunc <mjkunc@gmail.com>
2015-09-29 16:47:45 -0500

    initial 9 tests for game class complete

--- code/spec/game_spec.rb
+++ code/spec/game_spec.rb
@@ -1,25 +1,43 @@
-require_relative 'game'
+require_relative '../lib/game'
 
-describe 'Board' do
-  board = Board.new
+describe 'Game' do
+  game = Game.new
 
-  it 'returns a board object' do
-    expect(board).to be_an_instance_of Board
+  it 'returns a game object' do
+    expect(game).to be_an_instance_of Game
   end
 
-  it 'should have a length of 9' do
-    expect(board.board.length).to eq(9)
+  it 'should have a Board object' do
+    expect(game.instance_variable_get(:@board)).to be_an_instance_of Board
   end
 
-  it 'should contain only "-" ' do
-    expect(board.board.all? { |e| e == '-' }).to eq(true)
+  it 'should have X & O as tokens' do
+    expect(game.instance_variable_get(:@tokens)).to eq(%w(X O))
   end
-end
 
-describe 'Player' do
-  player = Player.new
+  it 'should have a player object for player 1' do
+    expect(game.instance_variable_get(:@player_1)).to be_an_instance_of Player
+  end
 
-  it 'returns a Player object' do
-    expect(player).to be_an_instance_of Player
+  it 'should have a player object for player 2' do
+    expect(game.instance_variable_get(:@player_2)).to be_an_instance_of Player
+  end
+
+  it 'should start with Round 1' do
+    expect(game.instance_variable_get(:@round)).to eq(1)
+  end
+
+  it 'should return false initially for game over' do
+    expect(game.game_over).to eq(false)
+  end
+
+  it 'should return true for game_over if round 10 is reached' do
+    game.instance_variable_set(:@round, 10)
+    expect(game.game_over).to eq(true)
+  end
+
+  it 'should return true for game_over if a winner is detrimined' do
+    game.instance_variable_set(:@winner, true)
+    expect(game.game_over).to eq(true)
   end
 end
